f82d06b0dc5f9ac60957296ac2443babf2adef54
N-Hidaya <nurul-h@hotmail.com>
2022-08-02 15:30:26 +0800

    cleaned codes

--- src/Register.js
+++ src/Register.js
@@ -1,10 +1,8 @@
 import {useRef, useState, useEffect} from "react";
 import {faCheck, faTimes, faInfoCircle} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {Col, Divider, Row} from "antd";
+import {Col, Row} from "antd";
 import './Register.css';
-
-import axios from "./api/axios";
 
 //user can have lower/upper/number/ - or _
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
